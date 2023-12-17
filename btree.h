284bc05518b463eac7f45a41f3784bc35c9b3b2f
Ivan Gelo <ivangelo@Ivans-MacBook-Pro.local>
2023-12-17 13:22:56 +0100

    move btree functions to btree file

--- btree.h
+++ btree.h
@@ -6,6 +6,14 @@
 
 uint32_t* leaf_node_num_cells(void* node);
 
+void* leaf_node_cell(void* node, uint32_t cell_num);
+
+uint32_t* leaf_node_key(void* node, uint32_t cell_num);
+
+void* leaf_node_value(void* node, uint32_t cell_num);
+
+void initialize_leaf_node(void* node);
+
 /*
  * Leaf Node Header Layout
  */
--- db.c
+++ db.c
@@ -14,22 +14,6 @@
 typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
 typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
 
-void* leaf_node_cell(void* node, uint32_t cell_num) {
-    return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
-}
-
-uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
-    return leaf_node_cell(node, cell_num);
-}
-
-void* leaf_node_value(void* node, uint32_t cell_num) {
-    return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
-}
-
-void initialize_leaf_node(void* node) {
-    *leaf_node_num_cells(node) = 0;
-}
-
 Cursor* table_end(Table* table) {
     Cursor* cursor = (Cursor*)malloc(sizeof(Cursor));
     cursor->table = table;
